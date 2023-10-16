6bf3d31cb7cb9617fcea3a3643a7313f96e4722a
Daniel <danieljoskowski6@wp.pl>
2023-10-16 18:51:22 +0200

    Aplikacja todo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    
-
     <div class="wrapper">
+      <!-- 
       <div>
         <b-nav>
           <b-nav-item active>Active</b-nav-item>
@@ -16,8 +15,6 @@ import HelloWorld from './components/HelloWorld.vue'
           <b-nav-item disabled>Disabled</b-nav-item>
         </b-nav>
       </div>
-      
-      
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -25,10 +22,17 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/contact">Contact</RouterLink>
         <RouterLink to="/maps">Maps</RouterLink>
         <RouterLink to="/images">Images</RouterLink>
-      </nav>
+      </nav> -->
+
+      <b-container class="bg-primary">
+        <b-row class="text-center">
+          <b-col cols="4">1 of 3</b-col>
+          <b-col cols="4">2 of 3</b-col>
+          <b-col cols="4">3 of 3</b-col>
+        </b-row>
+      </b-container>
     </div>
   </header>
-
   <RouterView />
 </template>
 
@@ -37,6 +41,4 @@ header {
   line-height: 1.5;
   max-height: 100vh;
 }
-
-
 </style>
