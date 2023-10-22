f40e226f2a81c95f1b557650accd11817d891354
Daniel <danieljoskowski6@wp.pl>
2023-10-22 19:58:22 +0200

    Dprzywrócenie poprawnych plików

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,65 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
+import "./assets/main.css";
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <!-- 
-      <div>
-        <b-nav>
-          <b-nav-item active>Active</b-nav-item>
-          <b-nav-item>Link</b-nav-item>
-          <b-nav-item>Another Link</b-nav-item>
-          <b-nav-item disabled>Disabled</b-nav-item>
+      <div
+        id="navbar"
+        class="d-flex justify-content-between align-items-center"
+      >
+        <!-- lewa część -->
+        <b-nav id="logo" class="d-flex align-items-center">
+          <div>
+            <b-navbar-brand to="/">
+              <!-- 'to' to link do strony głównej -->
+
+              <img src="logo.png" id="logo" alt="Logo" />
+              Duolingo
+            </b-navbar-brand>
+          </div>
         </b-nav>
+        <!-- prawa część -->
+
+        <div>
+          <b-nav id="menu">
+            <RouterLink to="/dictionary" class="link">Dictionary</RouterLink>
+            <RouterLink to="/account" class="link">Account</RouterLink>
+            <RouterLink to="/cart" class="link">Cart</RouterLink>
+            <RouterLink to="/contact" class="link">Contact</RouterLink>
+          </b-nav>
+        </div>
       </div>
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/contact">Contact</RouterLink>
-        <RouterLink to="/maps">Maps</RouterLink>
-        <RouterLink to="/images">Images</RouterLink>
-      </nav> -->
-
-      <b-container class="bg-primary">
-        <b-row class="text-center">
-          <b-col cols="4">1 of 3</b-col>
-          <b-col cols="4">2 of 3</b-col>
-          <b-col cols="4">3 of 3</b-col>
-        </b-row>
-      </b-container>
     </div>
   </header>
   <RouterView />
 </template>
 
 <style scoped>
+.link {
+  text-decoration: none;
+  margin: 20px;
+  color: red;
+}
+
+#navbar {
+  font-size: 20px;
+  max-height: 100px;
+}
+#logo {
+}
+#menu {
+}
+#logo {
+  height: 100px;
+}
 header {
   line-height: 1.5;
-  max-height: 100vh;
+}
+.wrapper {
+  background-color: #aaa;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,53 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import ContactView from '../views/ContactView.vue'
-import ImagesView from '../views/ImagesView.vue'
-import MapsView from '../views/MapsView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import Account from "../views/Account.vue";
+import Cart from "../views/Cart.vue";
+import Dictionary from "../views/Dictionary.vue";
+import Contact from "../views/Contact.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/contact',
-      name: 'contact',
-      component: ContactView
+      path: "/account",
+      name: "account",
+      component: Account,
     },
     {
-      path: '/maps',
-      name: 'maps',
-      component: MapsView
+      path: "/cart",
+      name: "cart",
+      component: Cart,
     },
     {
-      path: '/images',
-      name: 'images',
-      component: ImagesView
+      path: "/dictionary",
+      name: "dictionary",
+      component: Dictionary,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/contact",
+      name: "contact",
+      component: Contact,
+    },
+    // {
+    //   path: "/contact",
+    //   name: "contact",
+    //   component: ContactView,
+    // },
 
-export default router
+    // {
+    //   path: "/about",
+    //   name: "about",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import("../views/AboutView.vue"),
+    // },
+  ],
+});
+
+export default router;
