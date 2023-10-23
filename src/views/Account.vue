cd9f82ec2c8d3a0bba3b6694d092b2079009b9e4
Daniel <danieljoskowski6@wp.pl>
2023-10-23 21:58:11 +0200

    dodanie rejestracji uzytkownika

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,47 +1,52 @@
 <template>
-  <div class="auth-container">
-    <div class="auth-form" v-if="activeTab === 'login'">
+  <div>
+    <div v-if="!loggedIn">
       <h2>Logowanie</h2>
       <form @submit.prevent="login">
-        <label for="email">E-mail</label>
-        <input type="email" id="email" v-model="loginData.email" required />
-        <label for="password">Hasło</label>
+        <label for="loginUsername">Nazwa użytkownika</label>
+        <input
+          type="text"
+          id="loginUsername"
+          v-model="loginData.username"
+          required
+        />
+        <label for="loginPassword">Hasło</label>
         <input
           type="password"
-          id="password"
+          id="loginPassword"
           v-model="loginData.password"
           required
         />
         <button type="submit">Zaloguj się</button>
       </form>
-      <p>
-        Nie masz jeszcze konta?
-        <button @click="switchTab('register')">Zarejestruj się</button>
-      </p>
+      <p v-if="loginError" class="error">{{ loginError }}</p>
     </div>
 
-    <div class="auth-form" v-if="activeTab === 'register'">
+    <div v-else>
+      <p>Witaj, {{ loggedInUser.username }}!</p>
+      <button @click="logout">Wyloguj się</button>
+    </div>
+
+    <div>
       <h2>Rejestracja</h2>
       <form @submit.prevent="register">
-        <label for="regEmail">E-mail</label>
+        <label for="registerUsername">Nazwa użytkownika</label>
         <input
-          type="email"
-          id="regEmail"
-          v-model="registerData.email"
+          type="text"
+          id="registerUsername"
+          v-model="registerData.username"
           required
         />
-        <label for="regPassword">Hasło</label>
+        <label for="registerPassword">Hasło</label>
         <input
           type="password"
-          id="regPassword"
+          id="registerPassword"
           v-model="registerData.password"
           required
         />
         <button type="submit">Zarejestruj się</button>
       </form>
-      <p>
-        Masz już konto? <button @click="switchTab('login')">Zaloguj się</button>
-      </p>
+      <p v-if="registerError" class="error">{{ registerError }}</p>
     </div>
   </div>
 </template>
@@ -50,63 +55,71 @@
 export default {
   data() {
     return {
-      activeTab: "login",
       loginData: {
-        email: "",
+        username: "",
         password: "",
       },
       registerData: {
-        email: "",
+        username: "",
         password: "",
       },
+      loggedInUser: JSON.parse(localStorage.getItem("loggedInUser")) || null,
+      loginError: "",
+      registerError: "",
+      users: JSON.parse(localStorage.getItem("users")) || [],
     };
+  },
+  computed: {
+    loggedIn() {
+      return !!this.loggedInUser;
+    },
   },
   methods: {
     login() {
-      // Implementacja logowania
-      // Tutaj można wywołać odpowiednią akcję lub funkcję
+      // Sprawdź, czy użytkownik istnieje w tablicy users i hasło jest poprawne
+      const user = this.users.find(
+        (u) =>
+          u.username === this.loginData.username &&
+          u.password === this.loginData.password
+      );
+
+      if (user) {
+        this.loggedInUser = user;
+        this.loginError = "";
+        localStorage.setItem("loggedInUser", JSON.stringify(user));
+      } else {
+        this.loginError = "Nieprawidłowa nazwa użytkownika lub hasło";
+      }
+    },
+    logout() {
+      this.loggedInUser = null;
+      localStorage.removeItem("loggedInUser");
     },
     register() {
-      // Implementacja rejestracji
-      // Tutaj można wywołać odpowiednią akcję lub funkcję
-    },
-    switchTab(tab) {
-      this.activeTab = tab;
+      // Sprawdź, czy użytkownik o podanej nazwie już istnieje
+      const existingUser = this.users.find(
+        (u) => u.username === this.registerData.username
+      );
+
+      if (existingUser) {
+        this.registerError = "Użytkownik o tej nazwie już istnieje";
+      } else {
+        // Dodaj nowego użytkownika
+        this.users.push(this.registerData);
+        this.registerData = { username: "", password: "" };
+        this.registerError = "";
+
+        // Zapisz tablicę użytkowników w localStorage
+        localStorage.setItem("users", JSON.stringify(this.users));
+      }
     },
   },
 };
 </script>
 
 <style scoped>
-.auth-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-}
-
-.auth-form {
-  width: 300px;
-  padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  background-color: #f5f5f5;
-  text-align: center;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-  margin: 10px;
-}
-
-button {
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  padding: 10px 20px;
-  cursor: pointer;
-  border-radius: 5px;
-  margin-top: 10px;
-}
-
-button:hover {
-  background-color: #0056b3;
+.error {
+  color: red;
+  margin: 10px 0;
 }
 </style>
