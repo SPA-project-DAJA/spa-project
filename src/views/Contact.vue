a593801bd445c7ce2fb2e6170976f9b3c7709643
Daniel <danieljoskowski6@wp.pl>
2023-10-24 18:56:38 +0200

    Uzupełnienie zakładki kontakt

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,11 +2,37 @@
 
 <template>
   <main>
-    Company Name: Duolingo
-    <br />
-    123 Main Street <br />
-    Los Angeles, CA 90001 <br />
-    USA
+    <h1>Company Name: Duolingo</h1>
+    <p>
+      123 Main Street <br />
+      Los Angeles, CA 90001 <br />
+      USA
+    </p>
+
+    <h2>Contact Information:</h2>
+    <ul>
+      <li>Email: contact@duolingo.com</li>
+      <li>Phone: +1 (123) 456-7890</li>
+      <li>Website: <a href="https://www.duolingo.com">www.duolingo.com</a></li>
+    </ul>
+
+    <h2>Location on Google Maps:</h2>
+    <iframe
+      width="600"
+      height="450"
+      frameborder="0"
+      style="border: 0"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126771.84793017486!2d-118.243684!3d34.052235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe75705ff5435e88a!2sLos%20Angeles%2C%20California!5e0!3m2!1sen!2sus!4v1556753294394!5m2!1sen!2sus"
+      allowfullscreen
+    ></iframe>
+
+    <h2>About Us:</h2>
+    <p>
+      Duolingo is a language-learning platform that offers a fun and effective
+      way to learn new languages. With millions of users worldwide, we are
+      dedicated to helping people achieve their language learning goals.
+    </p>
+
     <TheWelcome />
   </main>
 </template>
